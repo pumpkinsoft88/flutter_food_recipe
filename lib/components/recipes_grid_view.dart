5ac7dab7b9af2bab94810b656628eaeacd59fab2
pumpkinsoft88 <pumpkinsoft88@gmail.com>
2023-02-22 06:50:10 +0900

    3rd commit

--- lib/components/recipes_grid_view.dart
+++ lib/components/recipes_grid_view.dart
@@ -5,11 +5,17 @@ import 'components.dart';
 class RecipesGridView extends StatelessWidget {
   const RecipesGridView({super.key});
 
+  static const _gridPadding = 24.0;
+
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      padding: const EdgeInsets.all(_gridPadding),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: _gridPadding,
+        crossAxisSpacing: _gridPadding,
+      ),
       itemBuilder: (BuildContext context, int index) {
         return const RecipeThubmnail();
       },
